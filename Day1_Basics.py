c3122145a846fdcf3ac4fdb3581c259fa153b88e
sahuamisha479-commits <sahuamisha479@gmail.com>
2025-10-25 20:09:41 +0530

    Create Day1_Basics.py

---
+++ Day1_Basics.py
@@ -0,0 +1,8 @@
+# Asking user for their name and age
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello,", name)
+print("You are", age, "years old.")
+print("Data type of name:", type(name))
+print("Data type of age:", type(age))
